f55b8b0e0a0ac965771ce2bf618596d23341e61a
tennismann3000 <47818246+tennismann3000@users.noreply.github.com>
2019-05-23 16:57:00 +0200

    changes
    23.05.
    - rearrangement.py Produktionsliste getrimmt und spalten umbenannt
    - script zur Säuberung der konvertierten py Datei aus ipynb

---
+++ .ipynb_checkpoints/process-checkpoint.py
@@ -0,0 +1,23 @@
+# def process(filename):
+#     """Removes empty lines and lines that contain only whitespace, and
+#     lines with comments"""
+#
+#     with open(filename) as in_file:
+#         out_file = open('testk2.py', 'w')
+#         for line in in_file:
+#             if not line.strip().startswith("#") and not line.isspace():
+#                 out_file.writelines(line)
+#
+# process('./testk.py')
+
+def process(filename, filename2):
+    """Removes empty lines and lines that contain only whitespace, and
+    lines with comments"""
+
+    with open(filename) as f1:
+        with open(filename2, 'w') as f2:
+            for line in f1:
+                if not line.strip().startswith("# ") and not line.isspace():
+                    f2.writelines(line)
+
+process('./testk.py', './testk2.py')
